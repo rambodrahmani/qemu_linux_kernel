74a00d1f677eb0407918b248db2ac4a1aef5e97c
Rambod Rahmani <rambodrahmani@autistici.org>
2019-07-14 23:42:58 +0200

    Clean up the source code. Add header comments.

--- lib/64/8259.s
+++ lib/64/8259.s
@@ -1,29 +1,45 @@
 ################################################################################
 # File: 8259.s
+#       The Intel 8259 is a Programmable Interrupt Controller (PIC) designed for
+#       the Intel 8085 and Intel 8086 microprocessors. The initial part was
+#       8259, a later A suffix version was upward compatible and usable with the
+#       8086 or 8088 processor. The 8259 combines multiple interrupt input
+#       sources into a single interrupt output to the host microprocessor,]
+#       extending the interrupt levels available in a system beyond the one or
+#       two levels found on the processor chip. The 8259A was the interrupt
+#       controller for the ISA bus in the original IBM PC and IBM PC AT. 
 #
 # Author: Rambod Rahmani <rambodrahmani@autistici.org>
 #         Created on 07/07/2019.
 ################################################################################
 
 #-------------------------------------------------------------------------------
-.set ICW1M, 0x20
-.set ICW2M, 0x21
-.set ICW3M, 0x21
-.set ICW4M, 0x21
-.set OCW1M, 0x21
-.set OCW3M, 0x20
-.set ICW1S, 0xa0
-.set ICW2S, 0xa1
-.set ICW3S, 0xa1
-.set ICW4S, 0xa1
-.set OCW1S, 0xa1
-.set OCW3S, 0xa0
-.set OCW2M, 0x20
-.set OCW3M, 0x20
-.set OCW2S, 0xa0
-.set OCW3S, 0xa0
+.set  ICW1M,  0x20
+.set  ICW2M,  0x21
+.set  ICW3M,  0x21
+.set  ICW4M,  0x21
+.set  OCW1M,  0x21
+.set  OCW3M,  0x20
+.set  ICW1S,  0xa0
+.set  ICW2S,  0xa1
+.set  ICW3S,  0xa1
+.set  ICW4S,  0xa1
+.set  OCW1S,  0xa1
+.set  OCW3S,  0xa0
+.set  OCW2M,  0x20
+.set  OCW3M,  0x20
+.set  OCW2S,  0xa0
+.set  OCW3S,  0xa0
+#-------------------------------------------------------------------------------
+.DATA
+ocw1m_save:
+    .BYTE 0
 
-	.global disable_8259
+ocw1s_save:
+    .BYTE 0
+#-------------------------------------------------------------------------------
+.GLOBAL disable_8259
+#-------------------------------------------------------------------------------
 disable_8259:
 	pushq %rbp
 	movq %rsp, %rbp
@@ -64,11 +80,12 @@ disable_8259:
 	popq %rax
 	leave
 	ret
-
+#-------------------------------------------------------------------------------
 // ripristina il controllore delle interruzioni, con la base compatibile con il DOS.
 // Inizializzo la modalita' fully nasted e i tipi base sono rispettivamente 0x08 per il master
 // e 0x70 per lo slave.
-		.global enable_8259
+.GLOBAL enable_8259
+#-------------------------------------------------------------------------------
 enable_8259:
 	pushq %rbp
 	movq %rsp, %rbp
@@ -105,9 +122,5 @@ enable_8259:
 	popq %rax
 	leave
 	ret
+#*******************************************************************************
 
-.data
-ocw1m_save:
-        .byte 0
-ocw1s_save:
-        .byte 0
