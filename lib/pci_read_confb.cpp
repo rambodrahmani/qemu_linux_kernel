5459a42ca20333cbcd01f75d6b561203adb6123f
Rambod Rahmani <rambodrahmani@autistici.org>
2019-08-23 18:31:51 +0200

    Clean up the source code and add comments.

--- lib/pci_read_confb.cpp
+++ lib/pci_read_confb.cpp
@@ -21,3 +21,71 @@ natb pci_read_confb(natb bus, natb dev, natb fun, natb off)
     return ret;
 }
 
+/**
+ * PCI Configuration Headers
+ * Every PCI device in the system, including the PCI-PCI bridges has a
+ * configuration data structure that is somewhere in the PCI configuration
+ * address space. The PCI Configuration header allows the system to identify
+ * and control the device. Exactly where the header is in the PCI Configuration
+ * address space depends on where in the PCI topology that device is. For
+ * example, a PCI video card plugged into one PCI slot on the PC motherboard
+ * will have its configuration header at one location and if it is plugged into
+ * another PCI slot then its header will appear in another location in PCI
+ * Configuration memory. This does not matter, for wherever the PCI devices and
+ * bridges are the system will find and configure them using the status and
+ * configuration registers in their configuration headers. Typically systems are
+ * designed so that every PCI slot has it's PCI Configuration Header in an
+ * offset that is related to its slot on the board. So, for example, the first
+ * slot on the board might have its PCI Configuration at offset 0 and the second
+ * slot at offset 256 (all headers are the same length, 256 bytes) and so on. A
+ * system specific hardware mechanism is defined so that the PCI configuration
+ * can attempt to examine all possible PCI Configuration Headers for a given PCI
+ * bus and know which devices are present and which devices are absent simply by
+ * trying to read one of the fields in the header (usually the Vendor
+ * Identification field) and getting some sort of error. The [3, PCI Local Bus
+ * Specification,] describes one possible error message as returning 0xFFFFFFFF
+ * when attempting to read the Vendor Identification and Device Identification
+ * fields for an empty PCI slot.
+ *
+ * Vendor Identification
+ *  A unique number describing the originator of the PCI device. Digital's PCI
+ *  Vendor Identification is 0x1011 and Intel's is 0x8086.
+ *
+ * Device Identification
+ *  A unique number describing the device itself. For example, Digital's 21141
+ *  fast ethernet device has a device identification of 0x0009.
+ *
+ * Status
+ *  This field gives the status of the device with the meaning of the bits of
+ *  this field set by the standard. [3, PCI Local Bus Specification,].
+ *
+ * Command
+ *  By writing to this field the system controls the device, for example
+ *  allowing the device to access PCI I/O memory,
+ *
+ * Class Code
+ *  This identifies the type of device that this is. There are standard classes
+ *  for every sort of device; video, scsi and so on. The class code for SCSI is
+ *  0x0100.
+ *
+ * Base Address Registers
+ *  These registers are used to determine and allocate the type, amount and
+ *  location of PCI I/O and PCI memory space that the device can use.
+ *
+ * Interrupt Pin
+ *  Four of the physical pins on the PCI card carry interrupts from the card to
+ *  the PCI bus. The standard labels these as A, B, C and D. The Interrupt Pin
+ *  field describes which of these pins this PCI device uses. Generally it is
+ *  hardwired for a pariticular device. That is, every time the system boots the
+ *  device uses the same interrupt pin. This information allows the interrupt
+ *  handling subsystem to manage interrupts from this device,
+ *
+ * Interrupt Line
+ *  The Interrupt Line field of the device's PCI Configuration header is used to
+ *  pass an interrupt handle between the PCI initialisation code, the device's
+ *  driver and Linux's interrupt handling subsystem. The number written there is
+ *  meaningless to the the device driver but it allows the interrupt handler to
+ *  correctly route an interrupt from the PCI device to the correct device
+ *  driver's interrupt handling code within the Linux operating system.
+ */
+
--- lib/pci_read_confw.cpp
+++ lib/pci_read_confw.cpp
@@ -21,3 +21,32 @@ natw pci_read_confw(natb bus, natb dev, natb fun, natb off)
     return ret;
 }
 
+/**
+ * PCI Address Spaces 
+ * The CPU and the PCI devices need to access memory that is shared between
+ * them. This memory is used by device drivers to control the PCI devices and to
+ * pass information between them. Typically the shared memory contains control
+ * and status registers for the device. These registers are used to control the
+ * device and to read its status. For example, the PCI SCSI device driver would
+ * read its status register to find out if the SCSI device was ready to write a
+ * block of information to the SCSI disk. Or it might write to the control
+ * register to start the device running after it has been turned on. The CPU's
+ * system memory could be used for this shared memory but if it were then every
+ * time a PCI device accessed memory, the CPU would have to stall waiting for
+ * the PCI device to finish. Access to memory is generally limited to one system
+ * component at a time. This would slow the system down. It is also not a good
+ * idea to allow the system's peripheral devices to access main memory in an
+ * uncontrolled way. This would be very dangerous; a rogue device could make the
+ * system very unstable.
+ *
+ * Peripheral devices have their own memory spaces which they use. The CPU can
+ * access these spaces but access by the devices into the system's memory is
+ * very strictly controlled using DMA (Direct Memory Access) channels. ISA
+ * devices have access to two address spaces, ISA I/O (Input/Output) and ISA
+ * memory. PCI has three; PCI I/O, PCI Memory and PCI Configuration space. All
+ * of these address spaces are also accessible by the CPU with the the PCI I/O
+ * and PCI Memory address spaces being used by the device drivers and the PCI
+ * Configuration space being used by the PCI initialization code within the
+ * Linux kernel.
+ */
+
