868f07b77e03a7a001087354da7143c549e3e012
Rambod Rahmani <rambodrahmani@autistici.org>
2019-08-23 17:39:06 +0200

    Clean up the source code and add comments.

--- lib/make_CAP.cpp
+++ lib/make_CAP.cpp
@@ -1,5 +1,26 @@
 /**
  * File: make_CAP.cpp
+ *       The CPU is not capable of initiating bus transactions in the
+ *       configuration space, and they must be generated indirectly using the
+ *       guest-PCI bridge. To this end, the bridge has 2 32-bits long registers
+ *       called CAP (Configuration Address Port) and CDP (Configuration Data
+ *       Port), placed in the I/O space at addresses 0x0CF8 and 0x0CFC.
+ *       In order to generate a transaction in the configuration space, the CPU
+ *       must
+ *          1. write a long word in the CAP register;
+ *          2. read/write a byte, a word or a long word from/in the CDP
+ *             register;
+ *          3. the double word to be written in the CAP register must contains
+ *             all the directives needed in order to address a long word in the
+ *             configuration space for a given function of a given device.
+ *       
+ *       When a valid long word is written in the CAP register, the guest-PCI
+ *       bridge will take care of executing a transaction in the configuration
+ *       space which will effectively take place when a read/write operation is
+ *       performed in the CDP register. When this happens, the guest-PCI bridge
+ *       will save:
+ *          1. the transaction type;
+ *          2. the byte enable values.
  *
  * Author: Rambod Rahmani <rambodrahmani@autistici.org>
  *         Created on 07/07/2019.
@@ -11,4 +32,12 @@ natl make_CAP(natb bus, natb dev, natb fun, natb off)
 {
 	return 0x80000000 | (bus << 16) | (dev << 11) | (fun << 8) | (off & 0xFC);
 }
+
+/**
+ * CAP Register
+ * 31               23           16 15    11 10   8 7            0
+ * ---------------------------------------------------------------
+ * |               |      Bus      | Device | Func. |   Offset   |
+ * ---------------------------------------------------------------
+ */
 
--- lib/pci_find_dev.cpp
+++ lib/pci_find_dev.cpp
@@ -35,3 +35,48 @@ bool pci_find_dev(natb& bus, natb& dev, natb& fun, natw deviceID, natw vendorID)
     return false;
 }
 
+/**
+ * A PCI Bus has 3 addressable spaces: memory, I/O and configuration.
+ * Each function of a device must implement, in the configuration space, certain
+ * registers having size from 1 to 4 bytes. Some of this register are
+ * compulsory:
+ *  1. Vendor ID: (read-only, 2 bytes, offset 0) identifies the vendor;
+ *  2. Device ID: (read-only, 2 bytes, offset 2) identifies the device;
+ *  3. Command Register (read/write, 2 bytes, offset 4):
+ *      - bit n. 0: if set, enables the function to accept transactions in the
+ *                  I/O space;
+ *      - bit n. 1: if set, enables the function to accept transactions in the
+ *                  memory space;
+ *      - bit n. 2: if set, enables the function to master the bus;
+ *  4. Status Register: (read-only, 2 bytes, offset 6) contains information
+ *                      regarding available functionalities and errors heppening
+ *                      during transactions;
+ *  5. Class Code: (read-only, 3 bytes, offset 9) defines the type of function
+ *                 carried out according to a standard PCI (see the table
+ *                 below);
+ *  6. Interrupt Pin: (read-only, 1 byte, offset 57) used to define the pin used
+ *                    to send interrupt requests.
+ */
+
+/**
+ * Standard PCI Class Codes:
+ *
+ * Class Code   |         PCI Device
+ *   (MSB)      |
+ * ---------------------------------------------
+ *    0x00      |       Pre revision 2.0
+ *    0x01      |    Mass storage controller
+ *    0x02      |      Network controller
+ *    0x03      |      Display controller
+ *    0x04      |       Multimedia device
+ *    0x05      |       Memory controller
+ *    0x06      |         Bridge device
+ *    0x07      | Simple communication controller
+ *    0x08      |      Base system peripheral
+ *    0x09      |          Input device
+ *    0x0A      |         Docking station
+ *    0x0B      |           Processor
+ *    0x0C      |       Serial bus controller
+ *    0xFF      |             Other
+ */
+
